91bd82b17d5fd243af688bcb61d5a910f75888a0
Mike V <mvee333@gmail.com>
2020-06-22 16:32:11 -0400

    built out file structure

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var connection = require("../config/connection");
+
+var orm = {
+  selectAll: function () {},
+  insertOne: function () {},
+  updateOne: function () {},
+};
+
+module.exports = orm;
