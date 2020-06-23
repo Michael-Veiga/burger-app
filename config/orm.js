0ad96e18e5f91bbf29510a3f402fbfab2b8fbc7b
Mike V <mvee333@gmail.com>
2020-06-23 14:00:55 -0400

    added methods to executive mysql commands in orm

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,38 @@
-var express = require("express");
 var connection = require("../config/connection");
-
+// create the methods that will execute the necessary MySQL commands in the controllers
 var orm = {
-  selectAll: function () {},
-  insertOne: function () {},
-  updateOne: function () {},
+  // select all from tableInput
+  selectAll: function (tableInput, cb) {
+    var queryString = "SELECT * FROM ??;";
+    connection.query(queryString, [tableInput], function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  // insert into table / column burger value
+  // give priority to col name and burger
+  insertOne: function (tableInput, colName, burgerName) {
+    queryString = "INSERT INTO ?? (??) VALUES (?)";
+    connection.query(queryString, [tableInput, colName, burgerName], function (
+      err,
+      result
+    ) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  // update table and set column with devoured boolean by id
+  updateOne: function (tableInput, colName, devoured, id, cb) {
+    queryString = "UPDATE ?? SET ?? = ? WHERE id = ?";
+    connection.query(
+      queryString,
+      [tableInput, colName, devoured, id],
+      function (err, result) {
+        if (err) throw err;
+        cb(result);
+      }
+    );
+  },
 };
 
 module.exports = orm;
