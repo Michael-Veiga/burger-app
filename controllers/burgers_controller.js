ea1d02fb4079b4c6aeb7f6567a5dde127bffe7b1
Mike V <mvee333@gmail.com>
2020-06-23 15:08:47 -0400

    created http methods and burger.js file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,26 @@
 var express = require("express");
 var router = express.Router();
+var burger = require("../models/burger");
+
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = { burgers: data };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/burger/create", function (req, res) {
+  burger.insertOne(req.body.burgerName),
+    function () {
+      res.redirect("/");
+    };
+});
+
+router.put("/burger/eat/:id", function (req, res) {
+  burger.updateOne(req.params.id, function () {
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,21 @@
 var orm = require("../config/orm");
 
+var burger = {
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
+
+  insertOne: function (burgerName, cb) {
+    orm.insertOne("burgers", "burger_name", burgerName, function (res) {
+      cb(res);
+    });
+  },
+  updateOne: function (id, cb) {
+    orm.updateOne("burgers", "devoured", true, id, function (res) {
+      cb(res);
+    });
+  },
+};
 module.exports = burger;
